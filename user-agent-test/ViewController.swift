e7157b8c73c1b20218cd85d2b1f7b445f7314097
000itsuki000 <itsuki.h@collabo-style.co.jp>
2023-12-19 17:40:37 +0900

    Initial Commit

---
+++ user-agent-test/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  user-agent-test
+//
+//  Created by Itsuki on 2023/12/19.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
